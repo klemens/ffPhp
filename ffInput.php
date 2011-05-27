a0d2607381bd3f80a93c37f4b94c8b5ce03e098f
Klemens Schölhorn <klemens@schoelhorn.eu>
2011-05-27 23:45:01 +0200

    Implemented IsComplete() and ApplySent() in ffInput

--- ffInput.php
+++ ffInput.php
@@ -5,14 +5,15 @@ class ffInput extends ffObject implements ffiControl{
                                          'label'    => array('type' => 'string'),
                                          'lines'    => array('type' => 'u+int', 'default' => 1),
                                          'cols'     => array('type' => 'u+int', 'default' => 15),
-                                         'maxlength'=> array('type' => 'uint', 'default' => -1),
+                                         'maxlength'=> array('type' => 'uint', 'default' => 0),
                                          'password' => array('type' => 'bool', 'default' => false),
                                          'value'    => array('type' => 'string', 'default' => ''),
                                          'flags'    => array('type' => 'array', 'default' => array()),
+                                         'regex'    => array('type' => 'string', 'default' => ''),
                                          'cssClass' => array('type' => 'array',  'default' => array()),
                                          'required' => array('type' => 'bool',  'default' => false),
                                          'error'    => array('type' => 'string',  'default' => ''),
-                                         'ffPhp'   => array('type' => 'object'));
+                                         'ffPhp'    => array('type' => 'object'));
     
     public function __construct($id = null, $label = null) {
         if(isset($id))
@@ -29,10 +30,23 @@ class ffInput extends ffObject implements ffiControl{
     }
     
     public function IsComplete() {
+        if($this->required && empty($this->ffPhp->req[$this->id])) {
+            $this->error = 'You have to fill in this field!';
+            return false;
+        }
         
+        if(!empty($this->ffPhp->req[$this->name]) && $this->regex &&
+           !preg_match($this->regex, $this->ffPhp->req[$this->name])) {
+            $this->error = 'Your input does not have the right format!'
+            return false;
+        }
+        
+        return true;
     }
     
     public function ApplySent() {
-
+        if(!$this->password && !empty($this->ffPhp->req[$this->name]) && empty($this->value)) {
+            $this->value = $this->ffPhp->req[$this->name];
+        }
     }
 }
