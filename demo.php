91ee6f44e1f07d8bfb0afffbd1ed1d76b4b723a7
Klemens Schölhorn <klemens@schoelhorn.eu>
2011-05-28 17:58:30 +0200

    Added demo.php with demo of ffPhp

---
+++ demo.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'ffPhp.php';
+
+$form = new ffPhp;
+
+$form->Add(new ffInput('name', 'Name'));
+
+if($form->IsSent()) {
+    echo '<a href="'.$form->action.'">Zurück</a>';
+    echo '<pre>';
+    var_dump($form->req);
+    echo '</pre>';
+} else {
+    $form->Show();
+}
